92b75f8a775360353689f26b8e381c9fe8a404e4
Michael Li <36088648+magicalpuffin@users.noreply.github.com>
2023-05-03 22:01:29 -0700

    remove debug

--- manufacturing_kanban/manufacturing_kanban/settings.py
+++ manufacturing_kanban/manufacturing_kanban/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'secretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
-# DEBUG = True
+DEBUG = (os.environ.get('DEBUG') == 'True')
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', 'localhost')]
 
