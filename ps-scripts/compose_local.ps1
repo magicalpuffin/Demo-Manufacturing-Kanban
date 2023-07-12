6f953543b132c7e91a42ada33b642eed7f583f87
Michael Li <36088648+magicalpuffin@users.noreply.github.com>
2023-07-11 21:35:40 -0700

    nuking everything

--- ps-scripts/compose_local.ps1
+++
@@ -1,4 +0,0 @@
-# Composes the local version
-. ps-scripts/env.ps1
-
-docker compose --file docker/docker-compose.yml --project-name $($PROJECT_NAME) up
--- ps-scripts/deploy_dev.ps1
+++
@@ -1,7 +0,0 @@
-. ps-scripts/env.ps1
-
-$ecrUri = "$($AWS_ACCOUNT_ID).dkr.ecr.$($AWS_REGION).amazonaws.com/$($ECR_REPO)"
-
-# Deploying ECR image with Zappa
-Write-Output "Zappa Deploying: $($ecrUri):$($TAG)"
-zappa deploy dev --docker-image-uri "$($ecrUri):$($TAG)"
