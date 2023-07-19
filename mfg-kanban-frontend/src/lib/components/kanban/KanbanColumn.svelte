a92f7bbaf73cd96aa49f4ff95598f466a88d45ca
Michael Li <36088648+magicalpuffin@users.noreply.github.com>
2023-07-18 19:59:59 -0700

    added basic sortablejs

--- mfg-kanban-frontend/src/lib/components/kanban/KanbanColumn.svelte
+++ mfg-kanban-frontend/src/lib/components/kanban/KanbanColumn.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
 	import type { LocationType, WorkOrderType } from '$lib/types';
+	import { onMount } from 'svelte';
 	import KanbanCard from './KanbanCard.svelte';
+	import Sortable from 'sortablejs';
 
 	export let Location: LocationType;
 	export let WorkOrders: WorkOrderType[];
+
+	let sortableEle: HTMLElement;
+	let sortableObj;
+
+	onMount(() => {
+		sortableObj = Sortable.create(sortableEle, {
+			group: 'location',
+			animation: 150,
+			ghostClass: 'blue-background-class',
+			onEnd: function (evt) {
+				console.log('sort ended');
+			}
+		});
+	});
 </script>
 
 <div class="border rounded-lg">
 	<h1 class="font-bold text-2xl py-2 px-2">{Location.name}</h1>
-	<div class="flex flex-col gap-2 px-2 py-2">
+	<div bind:this={sortableEle} class="flex flex-col gap-2 px-2 py-2">
 		{#each WorkOrders as WorkOrder}
 			<KanbanCard {WorkOrder} />
 		{/each}
---
+++ mfg-kanban-frontend/src/routes/test/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Sortable from 'sortablejs';
+	import { onMount } from 'svelte';
+
+	let testlist: HTMLElement;
+	onMount(() => {
+		let sortablelist = Sortable.create(testlist);
+	});
+</script>
+
+<ul bind:this={testlist}>
+	<li>item 1</li>
+	<li>item 2</li>
+	<li>item 3</li>
+</ul>
