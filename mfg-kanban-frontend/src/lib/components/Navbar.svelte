3cf201b1e43947f3c38ed1606b1df314523fe182
Michael Li <36088648+magicalpuffin@users.noreply.github.com>
2023-09-30 10:01:40 -0700

    added daisyui, updated navbar

--- mfg-kanban-frontend/src/lib/components/Navbar.svelte
+++ mfg-kanban-frontend/src/lib/components/Navbar.svelte
@@ -6,26 +6,27 @@
 	} from '$lib/stores/modal_stores';
 </script>
 
-<nav class="flex flex-row items-center gap-2 border-b py-2">
+<!-- <div class="flex flex-row items-center gap-2 border-b py-2"> -->
+<div class="navbar bg-base-100 gap-2 border-b">
 	<a class="px-4 py-2 text-xl font-bold hover:text-blue-400 md:text-2xl" href="/"
 		>Manufacturing Kanban</a
 	>
 	<button
-		class="rounded-xl border border-blue-600 bg-blue-600 px-2 py-2 text-white hover:bg-white hover:text-blue-600"
+		class="btn"
 		on:click={() => {
 			showCreateCardModal.set(true);
 		}}>Create Card</button
 	>
 	<button
-		class="rounded-xl border border-blue-600 bg-blue-600 px-2 py-2 text-white hover:bg-white hover:text-blue-600"
+		class="btn"
 		on:click={() => {
 			showLocationsModal.set(true);
 		}}>Locations</button
 	>
 	<button
-		class="rounded-xl border border-blue-600 bg-blue-600 px-2 py-2 text-white hover:bg-white hover:text-blue-600"
+		class="btn"
 		on:click={() => {
 			showPartsModal.set(true);
 		}}>Parts</button
 	>
-</nav>
+</div>
--- mfg-kanban-frontend/tailwind.config.js
+++ mfg-kanban-frontend/tailwind.config.js
@@ -4,5 +4,22 @@ export default {
 	theme: {
 		extend: {}
 	},
-	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#8476d3',
+					secondary: '#4efc74',
+					accent: '#1723d1',
+					neutral: '#1e1c26',
+					'base-100': '#fbf8fc',
+					info: '#5a8ae2',
+					success: '#186839',
+					warning: '#fdbd5e',
+					error: '#f4736c'
+				}
+			}
+		]
+	},
+	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('daisyui')]
 };
