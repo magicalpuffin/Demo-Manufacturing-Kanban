a9ca8284615671abfd1e7dad575d7536ed589857
Michael Li <mealted.lemon@gmail.com>
2023-11-12 15:51:38 -0800

    rename modal components

--- mfg-kanban-frontend/src/lib/components/navbar/Navbar.svelte
+++ mfg-kanban-frontend/src/lib/components/navbar/Navbar.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import { kanbanLocations, kanbanParts, kanbanWorkorders } from '$lib/stores/modal_stores';
-	import ManageLocationModal from '../modals/location_modal/ManageLocationModal.svelte';
-	import ManagePartModal from '../modals/part_modal/ManagePartModal.svelte';
-	import CreateWorkOrder from '../modals/workorder_modal/CreateWorkOrder.svelte';
-
-	import NavbarButton from './NavbarButton.svelte';
+	import LocationModal from '$lib/components/modals/locationModal/LocationModal.svelte';
+	import PartModal from '$lib/components/modals/partModal/PartModal.svelte';
+	import WorkOrderModal from '$lib/components/modals/workOrderModal/WorkOrderModal.svelte';
 </script>
 
 <div class="navbar bg-base-100 border-b">
@@ -12,13 +10,9 @@
 		<a class="btn btn-ghost text-xl" href="/">Manufacturing Kanban</a>
 	</div>
 	<div class="navbar-center gap-2">
-		<CreateWorkOrder
-			Parts={kanbanParts}
-			Locations={kanbanLocations}
-			Workorders={kanbanWorkorders}
-		/>
-		<ManageLocationModal Locations={kanbanLocations} WorkOrders={kanbanWorkorders} />
-		<ManagePartModal Parts={kanbanParts} WorkOrders={kanbanWorkorders} />
+		<WorkOrderModal Parts={kanbanParts} Locations={kanbanLocations} Workorders={kanbanWorkorders} />
+		<LocationModal Locations={kanbanLocations} WorkOrders={kanbanWorkorders} />
+		<PartModal Parts={kanbanParts} WorkOrders={kanbanWorkorders} />
 	</div>
 	<div class="navbar-end" />
 </div>
