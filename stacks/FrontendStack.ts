da687835351b8be8620e29ec5bc1b8c897533066
Michael Li <mealted.lemon@gmail.com>
2024-03-16 18:23:56 -0700

    added subdomain and deploy action

--- stacks/FrontendStack.ts
+++ stacks/FrontendStack.ts
@@ -3,15 +3,15 @@ import { APIStack } from "./APIStack";
 import { RDSStack } from "./RDSStack";
 
 export function FrontendStack({ app, stack }: StackContext) {
-  //   const { rds } = use(RDSStack);
   const { api } = use(APIStack);
 
-  // Requires two deploys, first without site
-  // https://github.com/sst/sst/issues/3698
   const frontend = new SvelteKitSite(stack, "frontend", {
-    // bind: [api, rds],
     path: "packages/frontend/",
     environment: { PUBLIC_API_URL: api.url },
+    customDomain: {
+      domainName: "demo.kanban.puffinsystems.com",
+      hostedZone: "puffinsystems.com",
+    },
   });
   stack.addOutputs({ FrontendUrl: frontend.url });
 }
